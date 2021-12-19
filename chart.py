b95d3c285debedcd4a98b3db183e0fb4b486e9fd
Hoai Bao <hoaibao.nhb@gmail.com>
2021-12-19 21:15:06 +0700

    update

---
+++ chart.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+pen = Turtle()
+win = Screen()
+
+pen.circle(100)
+pen.color('red')
+pen.fillcolor()
+pen.begin_fill()
+
+pen.begin_fill()
+pen.circle(100,30)
+pen.setposition(0,100)
+pen.setposition(0,0)
+pen.end_fill()
+
+win.exitonclick()
+
--- main.py
+++ main.py
@@ -4,64 +4,65 @@ import pandas as pd
 from time import sleep
 
 
-def adjust_per(users_dict,sub_users_dict,X,Y):
+def adjust_per(users_dict, sub_users_dict, X, Y):
 
-    
     for u in users_dict.keys():
         if u in sub_users_dict.keys():
-            users_dict[u] = users_dict[u]*X/100 + sub_users_dict[u]/(nav_after- realtime_nav)*Y
+            users_dict[u] = users_dict[u]*X/100 + \
+                sub_users_dict[u]/(nav_after - realtime_nav)*Y
         else:
-            users_dict[u] = users_dict[u]*X/100 +0/(nav_after- realtime_nav)*Y
+            users_dict[u] = users_dict[u]*X/100 + \
+                0/(nav_after - realtime_nav)*Y
     return users_dict
-
-
 
 
 if __name__ == '__main__':
     global base_path, data_path, output_path
-    Basic_nav = 570.4 *1000000
-    Future_nav = 90 *1000000
+    Basic_nav = 795.0 * 1000000
+    Future_nav = 0.0 * 1000000
     TODAY_NAV = Basic_nav+Future_nav
     base_path = os.getcwd()
-    data_path = os.path.join(base_path,'data.xlsx')
-    data_df = pd.read_excel(data_path, sheet_name=0, header=0,engine='openpyxl')
-    
-    
+    data_path = os.path.join(base_path, 'data.xlsx')
+    data_df = pd.read_excel(data_path, sheet_name=0,
+                            header=0, engine='openpyxl')
+
     print('TOTAL in Monney: {}'.format(data_df['Monney'].sum()))
     print('TODAY NAV: {}'.format(TODAY_NAV))
     print('TOTAL Revenue: {}'.format(TODAY_NAV-data_df['Monney'].sum()))
 
     date_list = list(set(data_df['Date'].tolist()))
     date_list.sort()
-    
-    #init user dict
-    users_dict = {}    
+
+    # init user dict
+    users_dict = {}
     users_list = list(set(data_df['ClientID'].tolist()))
     for u in users_list:
         users_dict[u] = 0
-    
+
     nav_after = 0
 
-    #MAIN LOOP: loop for all date in data
-    for idx,date_ in enumerate(date_list):
-        
-        sub_users_dict ={}
+    # MAIN LOOP: loop for all date in data
+    for idx, date_ in enumerate(date_list):
 
-        sub_data_df = data_df[data_df['Date']==date_list[idx]].reset_index()
+        sub_users_dict = {}
+
+        sub_data_df = data_df[data_df['Date'] == date_list[idx]].reset_index()
 
         realtime_nav = sub_data_df['RealTimeNAV'][0]
         nav_after = realtime_nav + sub_data_df['Monney'].sum()
 
-        print('{} -- RT: {} -- NAV_AF: {}'.format(date_,realtime_nav,nav_after))
+        print('{} -- RT: {} -- NAV_AF: {}'.format(date_, realtime_nav, nav_after))
 
         X = realtime_nav/nav_after*100
-        Y = (nav_after- realtime_nav)/nav_after*100
+        Y = (nav_after - realtime_nav)/nav_after*100
 
         for u in sub_data_df['ClientID'].tolist():
-            sub_users_dict[u] = sub_data_df[sub_data_df['ClientID']==u]['Monney'].sum()
+            sub_users_dict[u] = sub_data_df[sub_data_df['ClientID']
+                                            == u]['Monney'].sum()
 
-        users_dict = adjust_per(users_dict,sub_users_dict,X,Y)
+        users_dict = adjust_per(users_dict, sub_users_dict, X, Y)
 
-    #print output
+    # print output
     for u in users_dict.keys():
-        print('{} -- {}% -- {} -- {}'.format(u,round(users_dict[u],2),data_df[data_df['ClientID']==u]['Monney'].sum(),round(users_dict[u]*TODAY_NAV/100)))
+        print('{} -- {}% -- {} -- {} -- {}'.format(u, round(users_dict[u], 2), data_df[data_df['ClientID'] == u]['Monney'].sum(), round(
+            users_dict[u]*TODAY_NAV/100), round(users_dict[u]*TODAY_NAV/100)-data_df[data_df['ClientID'] == u]['Monney'].sum()))
